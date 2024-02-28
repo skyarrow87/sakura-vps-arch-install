2d2ebceed946bdd53c2df0177b66bdaf7d8129d6
skyarrow87 <80459567+skyarrow87@users.noreply.github.com>
2024-02-28 21:14:11 +0900

    Add chroot.sh script to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 sudo su
+cd /tmp
 wget https://mirrors.cat.net/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.gz
 tar -xzf archlinux-bootstrap-x86_64.tar.gz
+wget -P /tmp/root.x86_64/root/ https://raw.githubusercontent.com/soramikan/sakura-vps-arch-install/main/chroot.sh
 mount --bind /tmp/root.x86_64 /tmp/root.x86_64
-/tmp/root.x86_64/bin/arch-chroot /tmp/root.x86_64/
-
+/tmp/root.x86_64/bin/arch-chroot /tmp/root.x86_64/ /bin/zsh /tmp/root.x86_64/chroot.sh
